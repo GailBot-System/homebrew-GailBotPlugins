e1d89e7a9bda3c76023d90486a5b3b87454ea97c
Eva Caro <159644972+evacaro20@users.noreply.github.com>
2024-08-22 11:38:21 -0400

    Create gailbotplugins.rb

---
+++ Formula/gailbotplugins.rb
@@ -0,0 +1,17 @@
+class Gailbotplugins < Formula
+    desc "A tool to create, download, and upload plugins for GailBot"
+    homepage "https://github.com/GailBot-System/GailBotTools"
+    url "https://github.com/GailBot-System/GailBotPlugins/releases/download/v0.0.2/GailBotPlugins-v0.0.2.tar.gz"
+    sha256 "977276eefa9043dff9403ee58f9fe16125ecdf594d38a801d81ec122ec2c0076"
+    license "MIT"
+  
+    depends_on "python@3.9"
+  
+    def install
+    system "python3", "-m", "pip", "install", ".", "--prefix=#{prefix}", "--no-deps"
+    end
+  
+    test do
+      system "python3", "-c", "import GailBotPlugins; print('Installed successfully')"
+    end
+  end
